4f69e656c862a98b7751c8b388976667b8eedb8d
Kaustubh <kaustubhjadhav222@gmail.com>
2023-03-22 01:26:47 +0530

    What's New: 1) Delete Posts only if You Have Posted Them! 2) Blog Posts UI as Per Anjali and Our Blog Creation Logic Added

--- petme/lib/Widgets/blog_card.dart
+++ petme/lib/Widgets/blog_card.dart
@@ -8,59 +8,142 @@ class BlogCard extends StatelessWidget {
     Key? key,
     required this.snap,
   }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(26.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AppBar(
-            title: Text("Blogs"),
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-          ),
-          Container(
-            height: 150.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16.0),
-                topRight: Radius.circular(16.0),
+    return Column(
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        Material(
+          elevation: 5,
+          borderRadius: BorderRadius.circular(20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 270,
+                width: 350,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    //Image.
+                    Container(
+                      height: 200,
+                      width: 390,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                        ),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              snap['postImage'].toString()),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                              snap['title'],
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              style: const TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                              snap['description'],
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                        ),
+
+                      ],
+                    ),
+
+                  ],
+                ),
               ),
-              image: DecorationImage(
-                image: NetworkImage(snap['postImage'].toString()),
-                fit: BoxFit.cover,
-              ),
-                          ),
+
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  snap['title'],
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                SizedBox(height: 8.0),
-                Text(
-                  snap['description'],
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  snap['content'],
-                  style: Theme.of(context).textTheme.bodyText2,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 10,
+        )
+      ],
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Card(
+  //     margin: const EdgeInsets.symmetric(vertical: 10.0),
+  //     shape: RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.circular(26.0),
+  //     ),
+  //     child: Column(
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: [
+  //         AppBar(
+  //           title: Text("Blogs"),
+  //           backgroundColor: Colors.transparent,
+  //           elevation: 0.0,
+  //         ),
+  //         Container(
+  //           height: 150.0,
+  //           decoration: BoxDecoration(
+  //             borderRadius: BorderRadius.only(
+  //               topLeft: Radius.circular(16.0),
+  //               topRight: Radius.circular(16.0),
+  //             ),
+  //             image: DecorationImage(
+  //               image: NetworkImage(snap['postImage'].toString()),
+  //               fit: BoxFit.cover,
+  //             ),
+  //                         ),
+  //         ),
+  //         Padding(
+  //           padding: const EdgeInsets.all(16.0),
+  //           child: Column(
+  //             crossAxisAlignment: CrossAxisAlignment.start,
+  //             children: [
+  //               Text(
+  //                 snap['title'],
+  //                 style: Theme.of(context).textTheme.headline6,
+  //               ),
+  //               SizedBox(height: 8.0),
+  //               Text(
+  //                 snap['description'],
+  //                 style: Theme.of(context).textTheme.subtitle1,
+  //               ),
+  //               SizedBox(height: 16.0),
+  //               Text(
+  //                 snap['content'],
+  //                 style: Theme.of(context).textTheme.bodyText2,
+  //               ),
+  //             ],
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
--- petme/lib/screens/HomeScreen/Navigation/blogfeedpage.dart
+++ petme/lib/screens/HomeScreen/Navigation/blogfeedpage.dart
@@ -42,67 +42,92 @@ class _BlogFeedPageState extends State<BlogFeedPage> {
         title: Text('Blogs'),
         backgroundColor: Colors.pink[300],
       ),
-      backgroundColor:  Colors.white38,
+      backgroundColor: Colors.white38,
       body: StreamBuilder(
-              stream:
-                  FirebaseFirestore.instance.collection('Blogs').snapshots(),
-              builder: (context,
-                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
-                      child: CircularProgressIndicator(
-                    backgroundColor: Colors.pink[400],
-                  ));
-                }
-                return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    // final pet = provider[index];
-                    return BlogCard(
-                        snap: snapshot.data!.docs[index].data(),
-                      );
-                  },
-                );
-              },
-              ),
-
-
-
-      floatingActionButton: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CreateBlogPage()),
+        stream: FirebaseFirestore.instance.collection('Blogs').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+                child: CircularProgressIndicator(
+              backgroundColor: Colors.pink[400],
+            ));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+              // final pet = provider[index];
+              return BlogCard(
+                snap: snapshot.data!.docs[index].data(),
+              );
+            },
           );
         },
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 20,
-            horizontal: 10,
-          ),
-          child: Container(
-            width: 60,
-            height: 60,
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.pink[300],
-                  ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CreateBlogPage()),
+              );
+            },
+            label: Row(
+              children: const [
+                Icon(
+                  Icons.upload,
+                  size: 24,
                 ),
-                Center(
-                  child: Lottie.network(
-                    'https://assets3.lottiefiles.com/packages/lf20_we7qtj1g.json',
-                    repeat: true,
-                    fit: BoxFit.contain,
-                  ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  "Create Blog",
+                  style: TextStyle(fontSize: 17),
                 ),
               ],
             ),
-          ),
         ),
       ),
     );
   }
 }
+// floatingActionButton: GestureDetector(
+//   onTap: () {
+//     Navigator.push(
+//       context,
+//       MaterialPageRoute(builder: (context) => CreateBlogPage()),
+//     );
+//   },
+//   child: Padding(
+//     padding: const EdgeInsets.symmetric(
+//       vertical: 20,
+//       horizontal: 10,
+//     ),
+//     child: Container(
+//       width: 60,
+//       height: 60,
+//       child: Stack(
+//         children: [
+//           Container(
+//             decoration: BoxDecoration(
+//               shape: BoxShape.circle,
+//               color: Colors.pink[300],
+//             ),
+//           ),
+//           Center(
+//             child: Lottie.network(
+//               'https://assets3.lottiefiles.com/packages/lf20_we7qtj1g.json',
+//               repeat: true,
+//               fit: BoxFit.contain,
+//             ),
+//           ),
+//         ],
+//       ),
+//     ),
+//   ),
+// ),
+// );
+
+// }
